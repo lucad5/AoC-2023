2987df71ddceb4846a1f9d8560054b091410e710
lucad5 <87230523+lucad5@users.noreply.github.com>
2024-08-12 19:56:13 -0300

    Add solution for day 2

---
+++ day_2/tests/test_solution.py
@@ -0,0 +1,15 @@
+
+from solution import part_1
+
+
+def test_create_dictionary_based_on_input():
+    assert
+
+def test_ids_of_possible_games():
+    assert 
+
+def test_calculate_answer():
+    assert 
+
+def test_exception_is_raised_when_invalid_filename_is_provided():
+    assert
